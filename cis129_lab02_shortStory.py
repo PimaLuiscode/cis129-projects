3cff6b0eec0203df183cb8e21bc54c9299d05697
PimaLuiscode <lduarte16@mail.pima.edu>
2025-02-11 22:24:31 -0700

    Update cis129_lab02_shortStory.py
    edits the code to print a funny story instead

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!') 
+#fnunny story
+print('wake up') 
+print('take a poo')
+print('get out of bed')
+
+# Author: Luis Duarte
+# My first Python lab in my CIS129 course
+# Prints a simple short story 
